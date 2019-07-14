be7651d3a0327d24da4c54a962c387d33ac11be5
Funny Hat <010635@list.ru>
2019-07-14 21:34:50 +0300

    Add almost all of it

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Post
+from ckeditor.widgets import CKEditorWidget
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+    text = forms.CharField(widget=CKEditorWidget())
