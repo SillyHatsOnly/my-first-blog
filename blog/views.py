4371257be30df1d40a52133c8bf37c672cec6e84
Funny Hat <010635@list.ru>
2019-08-15 00:00:11 +0300

    not too good, but i'm still debugging

--- blog/views.py
+++ blog/views.py
@@ -21,56 +21,48 @@ class PostDetail(View):
 
 
 
+@login_required
+def post_new_or_edit(request, pk = None):
+
+    if pk is not None:
+        post = get_object_or_404(Post, pk=pk)
+
+    form = PostForm()
+
+    if pk is not None:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+#
 # @login_required
-# def post_new_or_edit(request, pk = None):
-#
-#     if pk is not None:
-#         post = get_object_or_404(Post, pk=pk)
-#
+# def post_new(request):
 #     if request.method == "POST":
 #         form = PostForm(request.POST)
-#         if pk is not None:
-#             form = PostForm(request.POST, instance=post)
 #         if form.is_valid():
 #             post = form.save(commit=False)
 #             post.author = request.user
 #             post.save()
 #             return redirect('post_detail', pk=post.pk)
-#         else:
-#             form = PostForm()
-#             if pk is not None:
-#                 form = PostForm(instance=post)
-#         return render(request, 'blog/post_edit.html', {'form': form})
-
-
-@login_required
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-
-
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+#
+#
+#
+# @login_required
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
 def post_draft_list(request):
